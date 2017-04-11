9f25a6a31c149c579bdc1829db299806ad5f56a5
GreenEyedAndy <cpmart@gmx.de>
2017-04-11 15:16:25 +0200

    Weitere Beispiele Chapter2 Objective5

---
+++ ExampleRunner/Chapter2Obj5.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using ConsoleDump;
+
+namespace ExampleRunner
+{
+    /// <summary>
+    /// Seeing whether an attribute is defined
+    /// </summary>
+    public class Example2_61 : Example
+    {
+        [Serializable]
+        class Person { }
+
+        public override void Run()
+        {
+            Attribute.IsDefined(typeof(Person), typeof(SerializableAttribute)).Dump();
+        }
+    }
+
+    /// <summary>
+    /// Adding properties to a custom attribute
+    /// </summary>
+    public class Example2_68 : Example
+    {
+        [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+        class CompleteCustomAttribute : Attribute
+        {
+            public CompleteCustomAttribute(string description)
+            {
+                Description = description;
+            }
+
+            public string Description { get; set; }
+        }
+
+        [CompleteCustom("hallo")]
+        class Person { }
+
+        public override void Run()
+        {
+            Attribute.IsDefined(typeof(Person), typeof(CompleteCustomAttribute)).Dump();
+        }
+    }
+}
